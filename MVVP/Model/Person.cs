ead55fb5e80f3e8f81ebbe19a574ccba36441e20
Bonu <cb185249@ncr.com>
2020-04-01 15:10:40 +0530

    WPF EXERCISE

---
+++ MVVP/Model/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVP.Model
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string fName;
+        
+        public string FName
+        {
+            get { return fName; }
+            set { fName = value; } 
+        }
+
+        private string lName;
+
+        public string LName
+        {
+            get { return lName; }
+            set { lName = value; }
+        }
+
+        public string fullName;
+
+        public string FullName
+        {
+            get { return fullName = FName + " " + LName; }
+            set {
+
+                if(fullName != value)
+                {
+                    fullName = value;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string p)
+        {
+            PropertyChangedEventHandler ph = PropertyChanged;
+            if (ph != null)
+            {
+                ph(this, new PropertyChangedEventArgs(p));
+            }
+        }
+    }
+}
---
+++ MVVP/ViewModel/PersonViewModel.cs
@@ -0,0 +1,84 @@
+﻿using MVVP.Command;
+using MVVP.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVP.ViewModel
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person personName;
+        
+
+        public Person PersonName
+        {
+            get { return personName; }
+            set {
+                personName = value;
+                NotifyPropertyChanged("PersonName");
+            }
+        }
+
+        private ObservableCollection<Person> _persons;
+
+        public PersonViewModel()
+        {
+            personName = new Person();
+            _persons = new ObservableCollection<Person>();
+
+        }
+
+        public ObservableCollection<Person> Persons
+        {
+            get { return _persons; }
+            set
+            {
+                _persons = value;
+                NotifyPropertyChanged("Persons");
+            }
+        }
+
+        private ICommand _submitCommand;
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (_submitCommand == null)
+                {
+                    _submitCommand = new RelayCommand(SubmitExecute, CanSubmitExecute, true);
+                }
+                return _submitCommand;
+            }
+        }
+       
+        private bool CanSubmitExecute(object parameter)
+        {
+            if (string.IsNullOrEmpty(personName.FName)  || string.IsNullOrEmpty(personName.LName))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void SubmitExecute(object parameter)
+        {
+            Persons.Add(PersonName);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
